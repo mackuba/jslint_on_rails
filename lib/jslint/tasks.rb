c4f059064532b0f139595aa6403d409ac83f8854
Jakub Suder <jakub.suder@gmail.com>
2009-12-17 22:09:33 +0100

    reset exclude_paths when paths is passed on command line

--- lib/jslint/tasks.rb
+++ lib/jslint/tasks.rb
@@ -2,10 +2,17 @@ require File.expand_path(File.dirname(__FILE__) + '/../jslint')
 
 desc "Run JSLint check on selected Javascript files"
 task :jslint do
-  lint = JSLint::Lint.new(
-    :paths => JSLint::Utils.paths_from_command_line('paths'),
-    :exclude_paths => JSLint::Utils.paths_from_command_line('exclude_paths')
-  )
+  include_paths = JSLint::Utils.paths_from_command_line('paths')
+  exclude_paths = JSLint::Utils.paths_from_command_line('exclude_paths')
+
+  if include_paths && exclude_paths.nil?
+    # if you pass paths= on command line but not exclude_paths=, and you have exclude_paths
+    # set in the config file, then the old exclude pattern will be used against the new
+    # include pattern, which may be very confusing...
+    exclude_paths = []
+  end
+
+  lint = JSLint::Lint.new :paths => include_paths, :exclude_paths => exclude_paths
   lint.run
 end
 
