a9c7e504bf5443b3b673fd3707cd6345acdda5bd
Jakub Suder <jakub.suder@gmail.com>
2010-01-16 17:34:57 +0100

    rails 3 compatibility fix

--- lib/jslint/rails.rb
+++ lib/jslint/rails.rb
@@ -1,4 +1,4 @@
 require 'jslint/utils'
 
 # for Rails, set config file path to config/jslint.yml in Rails root
-JSLint.config_path = File.expand_path(RAILS_ROOT + '/config/jslint.yml')
+JSLint.config_path = File.join(Rails.root, 'config', 'jslint.yml')
