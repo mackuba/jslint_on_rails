cfda1a427988c1227aef7e4b8b2045cc5aad026c
Harry Lascelles <harry@firstbanco.com>
2012-03-13 12:40:16 +0000

    Ensure jslint_on_rails doesn't create a new config file in the default location if the config path has already been set to point somewhere else.

--- lib/jslint/rails.rb
+++ lib/jslint/rails.rb
@@ -1,4 +1,8 @@
 require 'jslint/utils'
 
 # for Rails, set config file path to config/jslint.yml in Rails root
-JSLint.config_path = File.join(Rails.root, 'config', 'jslint.yml')
+if !JSLint.config_path.nil?
+  fail "JSLint config path #{JSLint.config_path} doesn't exist" if !File.exists?(JSLint.config_path)
+else
+  JSLint.config_path = File.join(Rails.root, 'config', 'jslint.yml')
+end
